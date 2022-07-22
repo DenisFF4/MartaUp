a93a71ab4ce59146586fba59eec21ccaf04df81b
DenisFF4 <106996707+DenisFF4@users.noreply.github.com>
2022-07-22 15:43:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: true,
+        autoplaySpeed: 4500,
+        verticalSwiping: true,
+    });
+
+});
